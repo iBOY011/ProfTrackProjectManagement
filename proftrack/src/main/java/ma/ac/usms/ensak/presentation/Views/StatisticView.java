af48230fb06b2053159a8e314418d51f04b65fc6
Majidied <majidimajidi2003@gmail.com>
2024-05-19 21:41:17 +0100

    Refactor StatisticView to improve code organization and readability

---
+++ proftrack/src/main/java/ma/ac/usms/ensak/presentation/Views/StatisticView.java
@@ -0,0 +1,24 @@
+package ma.ac.usms.ensak.presentation.Views;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class StatisticView extends VBox{
+    private HBox TotalHoursWork;
+    private HBox ProjectHoursWork;
+
+    public StatisticView() {
+        TotalHoursWork = new HBox();
+        ProjectHoursWork = new HBox();
+
+        getChildren().addAll(TotalHoursWork, ProjectHoursWork);
+    }
+
+    public HBox getTotalHoursWork() {
+        return TotalHoursWork;
+    }
+
+    public HBox getProjectHoursWork() {
+        return ProjectHoursWork;
+    }
+}
